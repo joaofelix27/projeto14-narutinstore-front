6aee204dd5a85f73fbd183f64e7a897743573b20
joaofelix27 <joaofelix27@gmail.com>
2022-07-07 19:25:24 -0300

    Inicio Cadastro

---
+++ src/Components/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { useState } from "react";
+import GlobalStyle from "../assets/globalstyle";
+import Login from "./Login";
+import Cadastro from "./Cadastro";
+import UserContext from "./UserContext";
+
+function App() {
+  const [token, setToken] = useState("");
+
+  const contextValue = { token, setToken };
+
+  return (
+    <>
+      <UserContext.Provider value={contextValue}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </>
+  );
+}
+
+export default App;
